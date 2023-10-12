bd0c271528e593ba4fb4c47d07a5045b7c1bc613
TiagoShebar <129063700+TiagoShebar@users.noreply.github.com>
2023-10-12 16:04:56 -0300

    Update site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,34 +1,54 @@
-﻿function VerificarPassword(password){
-    let largo = false;
-    let caracterEspecial = false;
-    let letraMayuscula = false;
-    for(let i = 0; i<password.length(); i++){
-        if(!letraMayuscula){
-            if(isLowerCase(password[i])){
-                letraMayuscula = true;
-            }
-        }
-        else if(!caracterEspecial){
-            if(contieneCaracterEspecial(password[i])){
-                caracterEspecial = true;
-            }
-        }
-    }
-    if(i > LargoMinimo){
-        largo = true;
-    }
+function ValidarFormularioOlvide(event) {
+    event.preventDefault();
 
-    return largo == caracterEspecial == letraMayuscula;
-   
+    let passwordInput = document.getElementsByName("NewPassword")[0].value;
+
+    if (VerificarPassword(passwordInput)) {
+        document.getElementById('registroForm').submit();
+    }
 }
 
 
-function contieneCaracterEspecial(char) {
-    var expresionRegular = /[!@#$%^&*(),.?":{}|<>]/;
-    return expresionRegular.test(char);
+function ValidarFormularioRegistro(event) {
+    event.preventDefault();
+
+    let passwordInput = document.getElementsByName("Password")[0].value;
+    let confirmPasswordInput = document.getElementsByName("Password2")[0].value;
+
+    if (VerificarPassword(passwordInput) && VerificarIgualdad(confirmPasswordInput)) {
+        document.getElementById('registroForm').submit();
+    }
 }
 
+function VerificarPassword(password) {
+    let largo = password.length >= 8;
+    let letraMayuscula = /[A-Z]/.test(password);
+    let caracterEspecial = /[!@#$%^&*(),.?":{}|<>]/.test(password);
 
-function VerificarRegistro(){
-    
+    let mensajeElement = document.getElementById('mensajePassword');
+
+    if (largo && letraMayuscula && caracterEspecial) {
+        mensajeElement.innerHTML = "La contraseña es válida";
+        mensajeElement.style.color = "green";
+        return true;
+    } else {
+        mensajeElement.innerHTML = "La contraseña debe tener al menos 8 caracteres, una letra mayúscula y un carácter especial.";
+        mensajeElement.style.color = "red";
+        return false;
+    }
+}
+
+function VerificarIgualdad(confirmPassword) {
+    let password = document.getElementsByName("Password")[0].value;
+    let mensajeElement = document.getElementById('mensajePassword2');
+
+    if (password === confirmPassword) {
+        mensajeElement.innerHTML = "Las contraseñas coinciden.";
+        mensajeElement.style.color = "green";
+        return true;
+    } else {
+        mensajeElement.innerHTML = "Las contraseñas no coinciden.";
+        mensajeElement.style.color = "red";
+        return false;
+    }
 }
