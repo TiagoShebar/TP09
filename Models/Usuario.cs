bd13c03b39e042b71c89b98e02e9260ea7716f6b
TiagoShebar <129063700+TiagoShebar@users.noreply.github.com>
2023-10-12 16:07:59 -0300

    Update Usuario.cs

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,8 +1,11 @@
-static class Usuario {
+public class Usuario {
     public int IdUsuario { get; set; }
     public string NombreUsuario { get; set; }
     public string Password { get; set; }
     public string Nombre { get; set; }
     public string Email { get; set; }
     public string Telefono { get; set; }
+    public int IdPregunta { get; set; }
+    public string Respuesta { get; set; }
+
 }
